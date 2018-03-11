c61ee4eef1994df6914b62bad6e554be43b63687
brelevenix <eric.debeau@gmail.com>
2018-03-11 18:26:25 +0100

    Create osm2communes.py

---
+++ import-osm/osm2communes.py
@@ -0,0 +1,35 @@
+# Python 2.7
+# Extract data from OSM and converts it to a json file
+# Each commune has the following format
+# {"lat": 48.812, "lng": -3.297, "name": "Penvénan"}
+
+import json
+import requests
+import sys
+
+# Parameters
+NB_DECIMALS = 3
+FLOAT_FORMAT = "{0:." + str(NB_DECIMALS) + "f}"
+
+URL = 'http://overpass-api.de/api/interpreter?data='
+OVERPASS_REQUEST = '[out:json];(area["ref:FR:SIREN"="200065928"]->.zone;' +\
+                   'node(area.zone)["ref:INSEE"~"22."]);out body;'
+
+
+def extract_overpass(overpass_url, overpass_request):
+    url = overpass_url + overpass_request
+    r = requests.get(url)
+    return (r.json())
+
+data = extract_overpass(URL, overpass_request)
+
+communes = []
+for elt in data["elements"]:
+    if elt["type"] == "node":
+        lat = float(FLOAT_FORMAT.format(elt["lat"]))
+        lon = float(FLOAT_FORMAT.format(elt["lon"]))
+        name = elt["tags"]["name"]
+        commune = {"lat": lat, "lng": lon, "name": name}
+        communes.append(commune)
+
+print json.dumps(communes, ensure_ascii=False)
