1d8cbdcb6c41b56c4489ef474b201e1ce5b3621a
Eric Debeau <eric.debeau@orange.com>
2020-11-15 14:22:23 +0100

    Improve generate_quiz.py
    Simplify input data
    Generate geodata files from names
    
    Signed-off-by: Eric Debeau <eric.debeau@orange.com>

--- generate_quiz.py
+++ generate_quiz.py
@@ -7,43 +7,109 @@
 # http://www.apache.org/licenses/LICENSE-2.0
 #
 
-""" Generate 3 files for geoquiz
-template directory must contain the following files:
+"""
+Generate 3 files for geoquiz.
+
+Template directory must contain the following files:
 - config.j2
 - geodata.j2
 - html.j2
 
-values file should be under values directory
+Value file(s) should be under values directory
+- name.yaml
+- name.geojson (optional)
 
-TODO: pass values as parameters
+Usage: generate_quiz <quiz_name>
+
+Generate 4 files:
+- <name>.bzh
+- geodata/<name>.js
+- config/<name>.js
+
+ToDo :
+- manage exceptions
+  - bad file name
+  - yaml input file with not mandatory parameters
+- log when errors
+
 """
 
-import json
+import sys
+from geojson import Point, Feature, FeatureCollection, dumps, load
+from geopy.geocoders import Nominatim
 from jinja2 import Environment, FileSystemLoader
 import yaml
 
-def render_file(template_file_name, value_file_name, output_file_name):
-    """Generate a file based on a jinja2 template"""
-    with open(value_file_name) as value_file:
-        extension = value_file_name[-4:]
-        if extension == "yaml":
-            config = yaml.load(value_file, Loader=yaml.FullLoader)
+
+def convert_name_to_geojson(names):
+    """Convert  array of names (string) to a geojson feature collection."""
+    geo_features = []
+    for name in names:
+        name = name.strip()
+        geolocator = Nominatim(user_agent="myGeocoder")
+        location = geolocator.geocode(name)
+        if location is not None:
+            properties = {"name": name}
+            geo_point = Point((location.latitude, location.longitude),
+                              precision=5)
+            geo_feature = Feature(geometry=geo_point, properties=properties)
+            geo_features.append(geo_feature)
         else:
-            geojson = json.load(value_file)
+            print("Error", name)
+    geo_fc = FeatureCollection(geo_features)
+    return geo_fc
 
-        file_loader = FileSystemLoader("templates")
-        env = Environment(loader=file_loader)
-        template = env.get_template(template_file_name)
 
-        if extension == "yaml":
-            output = template.render(config)
-        else:
-            output = template.render(geojson=geojson)
-        
-        file_value = open(output_file_name, "w")
-        file_value.write(output)
-        file_value.close()
+def render_file(template_file_name, values, output_file_name):
+    """Generate a file based on a jinja2 template."""
+    file_loader = FileSystemLoader("templates")
+    env = Environment(loader=file_loader, autoescape=True)
+    template = env.get_template(template_file_name)
+    output = template.render(values)
+    with open(output_file_name, "w") as file:
+        file.write(output)
 
-render_file("html.j2", "values/myquiz.yaml", "myquiz.html")
-render_file("config.j2", "values/myquiz.yaml", "config/config_myquiz.js")
-render_file("geodata.j2", "values/myquiz.geojson", "geodata/myquiz.js")
+
+def generate_quiz(name):
+    """Generate files for a quiz."""
+    input_file = name + ".yaml"
+
+    value_config = "./values/" + input_file
+    with open(value_config) as value_file:
+        values = yaml.safe_load(value_file, Loader=yaml.FullLoader)
+
+    # Generate HTML file
+    output_html_file = name + ".html"
+    render_file("html.j2", values, output_html_file)
+
+    # Generate configuration file
+    output_config_file = "./config/" + name + ".js"
+    render_file("config.j2", values, output_config_file)
+
+    # Generate data file
+    # Detect if list of locations or geoson file used
+    geo_locations = ""
+    if isinstance(values["locations"], list):
+        geo_locations = convert_name_to_geojson(values["locations"])
+    else:
+        file_name = "./values/" + values["locations"]
+        with open(file_name) as file:
+            geo_locations = load(file)
+
+    str_geo_locations = dumps(geo_locations)
+
+    # Workaround to add traps
+    if "traps" in values.keys():
+        for trap in values["traps"]:
+            search = '{"name": "' + trap + '"}'
+            replace = '{"name": "' + trap + '", "trap": "yes"}'
+            str_geo_locations = str_geo_locations.replace(search, replace)
+
+    values["geojson"] = str_geo_locations
+
+    output_geodata_file = "./geodata/" + name + ".js"
+    render_file("geodata.j2", values, output_geodata_file)
+
+
+if __name__ == "__main__":
+    generate_quiz(sys.argv[1])
--- geodata/myquiz.js
+++ geodata/myquiz.js
@@ -1,7 +1,1 @@
-var geo={"type": "FeatureCollection",
-"features": [
-    {"type": "Feature", "geometry": {"type": "Point", "coordinates": [-3.458799,48.7322183]}, "properties": {"name": "Lannion"}},
-    {"type": "Feature", "geometry": {"type": "Point", "coordinates": [-2.047687,48.4539775]}, "properties": {"name": "Dinan"}},
-    {"type": "Feature", "geometry": {"type": "Point", "coordinates": [-4.1024782,47.9960325]}, "properties": {"name": "Quimper"}},
-    {"type": "Feature", "geometry": {"type": "Point", "coordinates": [-1.6800198,48.1113387]}, "properties": {"name": "Rennes", "trap": "yes"}}
-]};
+var geo={"type": "FeatureCollection", "features": [{"type": "Feature", "geometry": {"type": "Point", "coordinates": [-3.4588, 48.73222]}, "properties": {"name": "Lannion", "trap": "yes"}}, {"type": "Feature", "geometry": {"type": "Point", "coordinates": [-2.04769, 48.45398]}, "properties": {"name": "Dinan"}}, {"type": "Feature", "geometry": {"type": "Point", "coordinates": [-4.10248, 47.99603]}, "properties": {"name": "Quimper"}}]}; 
